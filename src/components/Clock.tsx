9786da11742478f51e5840327632e143c87e35bb
Alex Bermúdez <alexis.gb249@gmail.com>
2025-02-23 21:04:46 -0500

    Clock now stops

--- src/components/Clock.tsx
+++ src/components/Clock.tsx
@@ -1,27 +1,34 @@
 import { useEffect, useState } from "react"
 
 var seconds:number = 10 * 60
+
 export function Clock() {
     const [time, setTime] = useState<string>("10:00")
 
-    const updateTime = () => {
-        seconds = seconds - 1
+    const updateTimer = () => {
+        seconds = seconds - 1;
         const date = new Date(0)
         date.setSeconds(seconds)
         setTime(date.toTimeString().slice(3, 8))
     }
 
+    const stopTimer = () => {
+        console.log("Time's up!")
+    }
+
     useEffect(() => {
+        if(seconds == 0) {
+            stopTimer()
+            return
+        }
         const interval = setInterval(() => {
-            updateTime()
+            updateTimer()
         }, 1000)
 
         return () => clearInterval(interval)
     })
 
     return (
-        <h1>
-            {time}
-        </h1>
+        <h1>{time}</h1>
     )
 }
