97816e9bc9d3f662e914667e94ec3df42c8cb08b
Alex Bermúdez <alexis.gb249@gmail.com>
2025-03-13 21:17:59 -0500

    Add styles to header, also imported bootstrap js

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,7 +1,20 @@
+import './Header.css'
+
 export const Header = () => {
     return (
-        <div className="">
-            Header
-        </div>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+                <div className="navbar-brand d-flex align-items-center prevent-select">
+                    <img className='logo me-2' src="/clock.png" alt="Pomodoro logo" />
+                    <h1 className='m-0'>Pomodoro</h1>
+                </div>
+                {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <div className="collapse navbar-collapse" id="navbarNav">
+                    Navbat content
+                </div> */}
+            </div>
+            </nav>
     )
 }
