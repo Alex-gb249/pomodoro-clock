8aaf5181bcab5248d7ae8113265526a0a7754743
Alex Bermúdez <alexis.gb249@gmail.com>
2025-03-24 16:58:42 -0500

    Separate utilities functions from component

---
+++ src/utilities/ClockUtils.ts
@@ -0,0 +1,19 @@
+export const DEFAULT_SECONDS = 25 * 60
+export const DEFAULT_BREAK_SECONDS = 5 * 60
+
+export const secondsToClock = (sec: number): string => {
+  const minutes = Math.floor(sec / 60).toString().padStart(2, '0')
+  const seconds = (sec % 60).toString().padStart(2, '0')
+  return `${minutes}:${seconds}`
+}
+
+export const initialSeconds = () => {
+  const savedSeconds = parseInt(localStorage.getItem('savedSeconds') || '0')
+  if(savedSeconds) return savedSeconds
+  return DEFAULT_SECONDS
+}
+
+export const getInitialIsBreak = () => {
+  const savedIsBreak = localStorage.getItem('isBreak') === 'true'
+  return savedIsBreak
+}
