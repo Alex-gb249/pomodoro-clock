e3751a005d5d2b92f45230f904d425be4c1c097e
Alex Bermúdez <alexis.gb249@gmail.com>
2025-03-30 22:26:16 -0500

    Add TaskManager component, dummy title and toggle button

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,18 @@ import './App.css'
 import { Header } from './components/header/Header'
 import { Clock } from './components/clock/Clock'
 import { Footer } from './components/footer/Footer'
+import { TaskManager } from './components/task-manager/TaskManager'
+import { useState } from 'react'
 
 function App() {
+  const [isSelectingTask, setIsSelectingTask] = useState(false)
 
   return (
     <>
       <div className='all'>
         <Header />
         <div className='d-flex justify-content-center align-items-center'>
-          <Clock />
+          {isSelectingTask ? <TaskManager /> : <Clock />}
         </div>
         <Footer />
       </div>
--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,20 +1,46 @@
 import './Header.css'
 
 export function Header() {
-    return (
-        <nav className="navbar navbar-expand-lg bg-body-tertiary">
-            <div className="container-fluid">
-                <div className="navbar-brand d-flex align-items-center prevent-select">
-                    <img className='logo me-2' src="/clock.png" alt="Pomodoro logo" />
-                    <h1 className='m-0'>Pomodoro</h1>
-                </div>
-                {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+  const handleClick = () => {}
+
+  return (
+    <nav className='navbar navbar-expand-lg bg-body-tertiary'>
+      <div className='container-fluid'>
+        <div className='navbar-brand d-flex align-items-center prevent-select'>
+          <img className='logo me-2' src='/clock.png' alt='Pomodoro logo' />
+          <h1>Pomodoro</h1>
+        </div>
+        <div className='d-flex align-items-center'>
+          <h3 className='me-2'>Dummy current task name</h3>
+          <a className='clickable' onClick={handleClick}>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='24'
+              height='24'
+              viewBox='0 0 24 24'
+              fill='none'
+              stroke='currentColor'
+              stroke-width='2'
+              stroke-linecap='round'
+              stroke-linejoin='round'
+            >
+              <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+              <path d='M3.5 5.5l1.5 1.5l2.5 -2.5' />
+              <path d='M3.5 11.5l1.5 1.5l2.5 -2.5' />
+              <path d='M3.5 17.5l1.5 1.5l2.5 -2.5' />
+              <path d='M11 6l9 0' />
+              <path d='M11 12l9 0' />
+              <path d='M11 18l9 0' />
+            </svg>
+          </a>
+        </div>
+        {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     Navbat content
                 </div> */}
-            </div>
-            </nav>
-    )
+      </div>
+    </nav>
+  )
 }
