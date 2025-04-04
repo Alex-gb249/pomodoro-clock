a62c947406a339f9800052db191f9a363c908fef
Alex Bermúdez <alexis.gb249@gmail.com>
2025-04-03 19:41:33 -0500

    Add pomodoro context for global state

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Clock } from './components/clock/Clock'
 import { Footer } from './components/footer/Footer'
 import { TaskManager } from './components/task-manager/TaskManager'
 import { useState } from 'react'
+import { PomodoroProvider } from './contexts/Pomodoro'
 
 function App() {
   const [isSelectingTask] = useState(false)
@@ -11,11 +12,13 @@ function App() {
   return (
     <>
       <div className='all'>
-        <Header />
-        <div className='d-flex justify-content-center align-items-center'>
-          {isSelectingTask ? <TaskManager /> : <Clock />}
-        </div>
-        <Footer />
+        <PomodoroProvider>
+          <Header />
+          <div className='d-flex justify-content-center align-items-center'>
+            {isSelectingTask ? <TaskManager /> : <Clock />}
+          </div>
+          <Footer />
+        </PomodoroProvider>
       </div>
     </>
   )
---
+++ src/contexts/Pomodoro.tsx
@@ -0,0 +1,57 @@
+import { createContext, useState } from 'react'
+import { getInitialIsBreak, initialSeconds } from '../utilities/ClockUtils'
+
+const DEFAULT_CONTEXT = {
+  seconds: initialSeconds(),
+  setSeconds: () => {},
+  isRunning: false,
+  setIsRunning: () => {},
+  isCustomizing: false,
+  setIsCustomizing: () => {},
+  customSeconds: initialSeconds(),
+  setCustomSeconds: () => {},
+  isBreak: getInitialIsBreak(),
+  setIsBreak: () => {},
+}
+
+export const PomodoroContext = createContext<PomodoroContextType>(DEFAULT_CONTEXT)
+
+export interface PomodoroContextType {
+  seconds: number
+  setSeconds: (seconds: number) => void
+  isRunning: boolean
+  setIsRunning: (isRunning: boolean) => void
+  isCustomizing: boolean
+  setIsCustomizing: (isCustomizing: boolean) => void
+  customSeconds: number
+  setCustomSeconds: (customSeconds: number) => void
+  isBreak: boolean
+  setIsBreak: (isBreak: boolean) => void
+}
+
+export function PomodoroProvider({ children }: { children: React.ReactNode }) {
+  const [seconds, setSeconds] = useState<number>(initialSeconds())
+  const [isRunning, setIsRunning] = useState<boolean>(false)
+  const [isCustomizing, setIsCustomizing] = useState<boolean>(false)
+  const [customSeconds, setCustomSeconds] = useState<number>(initialSeconds())
+  const [isBreak, setIsBreak] = useState<boolean>(getInitialIsBreak())
+
+  return (
+    <PomodoroContext.Provider
+      value={{
+        seconds,
+        setSeconds,
+        isRunning,
+        setIsRunning,
+        isCustomizing,
+        setIsCustomizing,
+        customSeconds,
+        setCustomSeconds,
+        isBreak,
+        setIsBreak,
+      }}
+    >
+      {children}
+    </PomodoroContext.Provider>
+  )
+}
