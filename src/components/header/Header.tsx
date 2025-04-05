dcd998941296e64f3184c104fc38256fa6601611
Alex Bermúdez <alexis.gb249@gmail.com>
2025-04-05 16:36:34 -0500

    Fix svg format

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -28,9 +28,9 @@ export function Header() {
                 viewBox='0 0 24 24'
                 fill='none'
                 stroke='currentColor'
-                stroke-width='2'
-                stroke-linecap='round'
-                stroke-linejoin='round'
+                strokeWidth='2'
+                strokeLinecap='round'
+                strokeLinejoin='round'
               >
                 <path stroke='none' d='M0 0h24v24H0z' fill='none' />
                 <path d='M3.5 5.5l1.5 1.5l2.5 -2.5' />
