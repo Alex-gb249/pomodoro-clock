f8e18ad4640c077cb78fb492db28b38d44a2c1f0
Alex Bermúdez <alexis.gb249@gmail.com>
2025-04-05 17:50:07 -0500

    Replace button with icons

--- src/components/task-manager/TaskManager.tsx
+++ src/components/task-manager/TaskManager.tsx
@@ -142,19 +142,61 @@ export function TaskManager() {
       <div className='card-body'>
         {isCreating ? (
           <>
-            <button className='btn btn-sm btn-success me-2' onClick={saveTask}>
-              Save
-            </button>
-            <button className='btn btn-sm btn-danger' onClick={handleCancel}>
-              Cancel
-            </button>
+            <a className='clickable text-success me-2' onClick={saveTask}>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='24'
+                height='24'
+                viewBox='0 0 24 24'
+                fill='none'
+                stroke='currentColor'
+                strokeWidth='2'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                className='icon icon-tabler icons-tabler-outline icon-tabler-check'
+              >
+                <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+                <path d='M5 12l5 5l10 -10' />
+              </svg>
+            </a>
+            <a className='clickable text-danger' onClick={handleCancel}>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='24'
+                height='24'
+                viewBox='0 0 24 24'
+                fill='none'
+                stroke='currentColor'
+                strokeWidth='2'
+                strokeLinecap='round'
+                strokeLinejoin='round'
+                className='icon icon-tabler icons-tabler-outline icon-tabler-x'
+              >
+                <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+                <path d='M18 6l-12 12' />
+                <path d='M6 6l12 12' />
+              </svg>
+            </a>
           </>
         ) : (
-          <>
-            <button className='btn btn-sm btn-success me-2' onClick={handleCreate}>
-              Create
-            </button>
-          </>
+          <a className='clickable text-success' onClick={handleCreate}>
+            <svg
+              xmlns='http://www.w3.org/2000/svg'
+              width='24'
+              height='24'
+              viewBox='0 0 24 24'
+              fill='none'
+              stroke='currentColor'
+              strokeWidth='2'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+              className='icon icon-tabler icons-tabler-outline icon-tabler-plus'
+            >
+              <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+              <path d='M12 5l0 14' />
+              <path d='M5 12l14 0' />
+            </svg>
+          </a>
         )}
       </div>
     </div>
