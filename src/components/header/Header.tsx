73d5e8a78c851e4c2764d56427b8d13394829c7a
Alex Bermúdez <alexis.gb249@gmail.com>
2025-04-05 16:30:40 -0500

    Hide current task header when selecting task

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -17,30 +17,32 @@ export function Header() {
           <img className='logo me-2' src='/clock.png' alt='Pomodoro logo' />
           <h1>Pomodoro</h1>
         </div>
-        <div className='d-flex align-items-center'>
-          <h3 className='me-2'>Dummy current task name</h3>
-          <a className='clickable' onClick={handleClick}>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              width='24'
-              height='24'
-              viewBox='0 0 24 24'
-              fill='none'
-              stroke='currentColor'
-              stroke-width='2'
-              stroke-linecap='round'
-              stroke-linejoin='round'
-            >
-              <path stroke='none' d='M0 0h24v24H0z' fill='none' />
-              <path d='M3.5 5.5l1.5 1.5l2.5 -2.5' />
-              <path d='M3.5 11.5l1.5 1.5l2.5 -2.5' />
-              <path d='M3.5 17.5l1.5 1.5l2.5 -2.5' />
-              <path d='M11 6l9 0' />
-              <path d='M11 12l9 0' />
-              <path d='M11 18l9 0' />
-            </svg>
-          </a>
-        </div>
+        {!isSelectingTask && (
+          <div className='d-flex align-items-center'>
+            <h3 className='me-2'>Dummy current task name</h3>
+            <a className='clickable' onClick={handleClick}>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                width='24'
+                height='24'
+                viewBox='0 0 24 24'
+                fill='none'
+                stroke='currentColor'
+                stroke-width='2'
+                stroke-linecap='round'
+                stroke-linejoin='round'
+              >
+                <path stroke='none' d='M0 0h24v24H0z' fill='none' />
+                <path d='M3.5 5.5l1.5 1.5l2.5 -2.5' />
+                <path d='M3.5 11.5l1.5 1.5l2.5 -2.5' />
+                <path d='M3.5 17.5l1.5 1.5l2.5 -2.5' />
+                <path d='M11 6l9 0' />
+                <path d='M11 12l9 0' />
+                <path d='M11 18l9 0' />
+              </svg>
+            </a>
+          </div>
+        )}
         {/* <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
