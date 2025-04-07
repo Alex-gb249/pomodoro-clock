53f7cd76b8096fc27b0720e8ede3306b57ebbd0e
Juan Giraldo <juanegiraldoa@proton.me>
2025-04-06 20:12:17 -0500

    Add replace svg

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -2,6 +2,7 @@ import { useContext } from 'react'
 import './Header.css'
 import { PomodoroContext } from '../../contexts/Pomodoro'
 import { TasksContext } from '../../contexts/Tasks'
+import Check from '../../assets/Check'
 
 export function Header() {
   const { isSelectingTask, setIsSelectingTask } = useContext(PomodoroContext)
@@ -23,25 +24,7 @@ export function Header() {
           <div className='d-flex align-items-center'>
             {currentTask && <h3 className='me-2'>{currentTask.getName()}</h3>}
             <a className='clickable' onClick={handleClick}>
-              <svg
-                xmlns='http://www.w3.org/2000/svg'
-                width='24'
-                height='24'
-                viewBox='0 0 24 24'
-                fill='none'
-                stroke='currentColor'
-                strokeWidth='2'
-                strokeLinecap='round'
-                strokeLinejoin='round'
-              >
-                <path stroke='none' d='M0 0h24v24H0z' fill='none' />
-                <path d='M3.5 5.5l1.5 1.5l2.5 -2.5' />
-                <path d='M3.5 11.5l1.5 1.5l2.5 -2.5' />
-                <path d='M3.5 17.5l1.5 1.5l2.5 -2.5' />
-                <path d='M11 6l9 0' />
-                <path d='M11 12l9 0' />
-                <path d='M11 18l9 0' />
-              </svg>
+              <Check />
             </a>
           </div>
         )}
